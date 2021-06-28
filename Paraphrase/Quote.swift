968548366a317e95683eaed6af85a7bf8cd752f9
Philip Modin <18331871+philmodin@users.noreply.github.com>
2021-06-28 14:55:51 -0700

    Completed "How to refactor your app to add unit tests" at: https://www.hackingwithswift.com/articles/94/how-to-refactor-your-app-to-add-unit-tests

--- Paraphrase/Quote.swift
+++ Paraphrase/Quote.swift
@@ -12,6 +12,36 @@ struct Quote: Codable, Comparable {
     var author: String
     var text: String
 
+    var singleLine: String {
+        let formattedText = text.replacingOccurrences(of: "\n", with: " ")
+        return "\(author): \(formattedText)"
+    }
+    
+    var multiline: String {
+        return "\"\(text)\"\n   — \(author)"
+    }
+    
+    var attributedString: NSAttributedString {
+        var textAttributes = [NSAttributedStringKey: Any]()
+        var authorAttributes = [NSAttributedStringKey: Any]()
+
+        if let quoteFont = UIFont(name: "Georgia", size: 24) {
+            let fontMetrics = UIFontMetrics(forTextStyle: .headline)
+            textAttributes[.font] = fontMetrics.scaledFont(for: quoteFont)
+        }
+
+        if let authorFont = UIFont(name: "Georgia-Italic", size: 16) {
+            let fontMetrics = UIFontMetrics(forTextStyle: .body)
+            authorAttributes[.font] = fontMetrics.scaledFont(for: authorFont)
+        }
+
+        let finishedQuote = NSMutableAttributedString(string: text, attributes: textAttributes)
+        let authorString = NSAttributedString(string: "\n\n\(author)", attributes: authorAttributes)
+        finishedQuote.append(authorString)
+
+        return finishedQuote
+    }
+    
     static func <(lhs: Quote, rhs: Quote) -> Bool {
         return lhs.author < rhs.author
     }
